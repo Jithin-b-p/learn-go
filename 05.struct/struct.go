3d36626cf0ece9eae413732d1bd59c92352f962e
unknown <jithinbp007@gmail.com>
2025-06-14 19:04:43 +0530

    creation contructor function

--- 05.struct/struct.go
+++ 05.struct/struct.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -16,20 +17,36 @@ type user struct {
 func (u user) outputUserDetails() {
 	fmt.Println(u.firstName, u.lastName, u.birthDate)
 }
+
+// returns the pointer so that we can mutate.
+func newUser(firstName, lastName, birthDate string) (*user, error) {
+
+	// adding validation
+	if firstName == "" || lastName == "" || birthDate == "" {
+
+		return nil, errors.New("first name, last name and birthdate are required")
+
+	}
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}, nil
+}
+
 func main() {
 
 	firstName := getUserData("Enter your first name: ")
 	lastName := getUserData("Enter your last name: ")
 	birthDate := getUserData("Enter your birthdate: ")
 
-	appUser := user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthDate,
-		createdAt: time.Now(),
-	}
-
+	appUser, err := newUser(firstName, lastName, birthDate)
 	// outputUserDetails(appUser)
+
+	if err != nil {
+		panic(err)
+	}
 
 	// passing address
 	appUser.outputUserDetails()
@@ -42,7 +59,7 @@ func main() {
 func getUserData(prompt string) (data string) {
 
 	fmt.Print(prompt)
-	fmt.Scan(&data)
+	fmt.Scanln(&data)
 
 	return
 
