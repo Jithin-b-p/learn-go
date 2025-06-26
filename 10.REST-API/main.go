bbdfcab7e472f7e4450e552c8da8bb282968557a
unknown <jithinbp007@gmail.com>
2025-06-26 14:10:31 +0530

    added a get and post events endpoints

--- 10.REST-API/main.go
+++ 10.REST-API/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/Jithin-b-p/learn-go/10.REST-API/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/user", func(context *gin.Context) {
-		context.JSON(200, gin.H{"name": "Jithin B P"})
-	})
+	r.GET("/events", getEvents)
+	r.POST("/events", createEvent)
 
 	r.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+
+	events := models.GetAllEvents()
+
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+
+	var event models.Event
+
+	if err := context.ShouldBindJSON(&event); err != nil {
+
+		context.JSON(http.StatusBadRequest, gin.H{"message": "value error"})
+	} else {
+
+		event.Save()
+		context.JSON(http.StatusCreated, gin.H{"message": "Event created"})
+	}
 }
--- 10.REST-API/models/event.go
+++ 10.REST-API/models/event.go
@@ -3,16 +3,20 @@ package models
 import "time"
 
 type Event struct {
-	Id          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
-	UserId      int
+	Id          int       `json:"id"`
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"datetime"`
+	UserId      int       `json:"userid"`
 }
 
 var events = []Event{}
 
 func (e Event) Save() {
 	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
